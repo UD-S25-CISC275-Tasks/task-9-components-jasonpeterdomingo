9691d95c20177e658325d7ffcab884ac588c9d2e
Jason Domingo <jayprdom@udel.edu>
2025-03-07 22:16:33 -0500

    completed CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,41 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    type Holiday = "🎄" | "🎆" | "❄️" | "🐰" | "🦃";
+    const ALPHABETICAL: Record<Holiday, Holiday> = {
+        "❄️": "🎄",
+        "🎄": "🐰",
+        "🐰": "🎆",
+        "🎆": "🦃",
+        "🦃": "❄️"
+    };
+    const YEAR: Record<Holiday, Holiday> = {
+        "🎆": "❄️",
+        "❄️": "🐰",
+        "🐰": "🦃",
+        "🦃": "🎄",
+        "🎄": "🎆"
+    };
+    const [holiday, setHoliday] = useState<Holiday>("❄️");
+    return (
+        <div>
+            <span>Holiday: {holiday}</span>
+            <div>
+                <Button
+                    onClick={() => {
+                        setHoliday(ALPHABETICAL[holiday]);
+                    }}
+                >
+                    Advance by Alphabet
+                </Button>
+                <Button
+                    onClick={() => {
+                        setHoliday(YEAR[holiday]);
+                    }}
+                >
+                    Advance by Year
+                </Button>
+            </div>
+        </div>
+    );
 }
