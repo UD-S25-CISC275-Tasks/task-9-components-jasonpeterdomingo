8d489a0c253c6bbe768256d2f5cbf894d3e8c53f
Jason Domingo <jayprdom@udel.edu>
2025-03-11 19:26:45 -0400

    Fixed DoubleHalf and deleted DoubleHalfState

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
+import { DoubleHalf } from "./bad-components/DoubleHalf";
 
 function App(): React.JSX.Element {
     return (
@@ -13,6 +14,14 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <hr></hr>
+            <DoubleHalf></DoubleHalf>
+            <hr></hr>
+            {/* <ChooseTeam></ChooseTeam> */}
+            <hr></hr>
+            {/* <ColoredBox></ColoredBox> */}
+            <hr></hr>
+            {/* <ShoveBox></ShoveBox> */}
             <hr></hr>
             <Counter></Counter>
             <hr />
